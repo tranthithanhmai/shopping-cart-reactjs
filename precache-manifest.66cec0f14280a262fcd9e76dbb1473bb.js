6551531483776a66d3d5ad02a3095f0464c74585
thanhmai <tranthithanhmai134@gmail.com>
2019-05-27 16:37:20 +0700

    Updates

---
+++ precache-manifest.66cec0f14280a262fcd9e76dbb1473bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3014f458ff84e082bb7ae9a3e2af3b97",
+    "url": "/shopping-cart-reactjs/index.html"
+  },
+  {
+    "revision": "448fa9822c0b97194ec2",
+    "url": "/shopping-cart-reactjs/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6b4303a403880436fb53",
+    "url": "/shopping-cart-reactjs/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "448fa9822c0b97194ec2",
+    "url": "/shopping-cart-reactjs/static/js/2.5a3c43ff.chunk.js"
+  },
+  {
+    "revision": "6b4303a403880436fb53",
+    "url": "/shopping-cart-reactjs/static/js/main.1ccc679e.chunk.js"
+  },
+  {
+    "revision": "82d0261cb2a1857f8b80",
+    "url": "/shopping-cart-reactjs/static/js/runtime~main.b03dee8c.js"
+  }
+]);
